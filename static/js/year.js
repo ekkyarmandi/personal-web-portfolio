c9b69d15b322eedc20d0e2e52cde34543042af0a
ekkyarmandi <ekkyarmandi@gmail.com>
2022-07-16 10:52:43 +0700

    adding year javascript function

---
+++ static/js/year.js
@@ -0,0 +1,14 @@
+function countYear() {
+    let year = document.querySelector("#starting-year");
+    let freelance = document.querySelector("#init-year");
+    let x = new Date(year.getAttribute('date'));
+    let y = new Date(freelance.getAttribute('date'));
+    console.log(y);
+    let d = new Date();
+    let year_diff1 = d.getFullYear() - x.getFullYear();
+    let year_diff2 = d.getFullYear() - y.getFullYear();
+    year.innerHTML = year_diff1;
+    freelance.innerHTML = year_diff2;
+}
+
+countYear();
